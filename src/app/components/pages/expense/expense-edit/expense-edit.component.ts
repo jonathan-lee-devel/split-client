ed591248b6ee17c3d04cf58dbd4aa2640aae82b4
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-06 20:23:10 +0100

    Implemented update/patch expenses

---
+++ src/app/components/pages/expense/expense-edit/expense-edit.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import {
+  ExpenseFrequency,
+} from '../../../../dtos/expenses/enum/ExpenseFrequency';
+import {FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ExpenseService} from '../../../../services/expense/expense.service';
+import {ModalService} from '../../../../services/modal/modal.service';
+import {ExpenseSelection} from '../expense-create/expense-create.component';
+
+@Component({
+  selector: 'app-expense-edit',
+  templateUrl: './expense-edit.component.html',
+  styleUrls: ['./expense-edit.component.css'],
+})
+export class ExpenseEditComponent implements OnInit {
+  expenseId: string = '';
+  title: string = '';
+  amount: number = 0;
+  frequency: ExpenseFrequency = ExpenseFrequency.ONCE;
+  expenseSelections: ExpenseSelection[] = [
+    {value: 0, viewValue: ExpenseFrequency[ExpenseFrequency.ONCE]},
+    {value: 1, viewValue: ExpenseFrequency[ExpenseFrequency.DAILY]},
+    {value: 2, viewValue: ExpenseFrequency[ExpenseFrequency.WEEKLY]},
+    {value: 3, viewValue: ExpenseFrequency[ExpenseFrequency.BIWEEKLY]},
+    {value: 4, viewValue: ExpenseFrequency[ExpenseFrequency.MONTHLY]},
+    {value: 5, viewValue: ExpenseFrequency[ExpenseFrequency.YEARLY]},
+  ];
+  propertyId: string = '';
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private expenseService: ExpenseService,
+    private modalService: ModalService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.expenseId = params['expenseId'];
+      this.expenseService.getExpense(this.expenseId).subscribe((expense) => {
+        this.title = expense.title;
+        this.amount = Number(expense.amount.slice(1, expense.amount.length));
+        this.frequency = expense.frequency;
+        this.range.setValue({
+          'start': expense.startDate, 'end': expense.endDate,
+        });
+        this.propertyId = expense.propertyId;
+      });
+    });
+  }
+
+  doUpdateExpense() {
+    this.expenseService.updateExpense(
+        this.expenseId,
+        this.propertyId,
+        this.title,
+        this.amount,
+        this.frequency,
+        this.range.get('start')?.value,
+        this.range.get('end')?.value,
+    ).subscribe((_) => {
+      this.modalService.showModal(
+          'Expense Creation',
+          `Expense \'${this.title}\' successfully updated`,
+      );
+      this.router.navigate([`/property/manage/${this.propertyId}/expenses`]);
+    });
+  }
+}
