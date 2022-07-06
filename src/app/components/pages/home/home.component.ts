03c0de96075b70557ef644a8b0ca44e3e071df43
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-06 16:10:59 +0100

    Replaced home page with notifications screen

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NotificationDto} from '../../../dtos/notifications/NotificationDto';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import {Component, OnInit} from '@angular/core';
  * Home component.
  */
 export class HomeComponent implements OnInit {
+  notifications: NotificationDto[] = [];
   /**
    * Standard constructor.
    */
@@ -19,5 +21,40 @@ export class HomeComponent implements OnInit {
    * Init function.
    */
   ngOnInit(): void {
+  }
+
+  calculateTimeDifference(datetime: Date): string {
+    let timeMessage = HomeComponent.timeSince(datetime);
+    if (timeMessage === '0 seconds ago') {
+      timeMessage = 'Just now';
+    }
+    return timeMessage;
+  }
+
+  private static timeSince(date: Date): string {
+    const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + ' years ago';
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + ' months ago';
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + ' days ago';
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + ' hours ago';
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + ' minutes ago';
+    }
+    return Math.floor(seconds) + ' seconds ago';
   }
 }
---
+++ src/app/dtos/notifications/NotificationDto.ts
@@ -0,0 +1,7 @@
+export interface NotificationDto {
+  title: string;
+  text: string;
+  datetime: Date;
+  routerLink: string;
+  buttonText: string;
+}
