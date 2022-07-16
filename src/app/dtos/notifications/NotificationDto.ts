76edfa53c88824b95504d7ab4bace5d71b78c821
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-07-16 05:46:32 +0100

    SPLIT-40 Modified NotificationDto to include routerLink and buttonText

--- src/app/dtos/notifications/NotificationDto.ts
+++ src/app/dtos/notifications/NotificationDto.ts
@@ -2,6 +2,6 @@ export interface NotificationDto {
   title: string;
   text: string;
   datetime: Date;
-  routerLink: string;
-  buttonText: string;
+  routerLink: string | undefined;
+  buttonText: string | undefined;
 }
