2bbf0a7da59e99e9553c571a3c0688e8df50e9c1
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2023-04-06 20:36:36 +0100

    SPLIT-75 Added functionality to allow admins to make other tenants admins (#15)

--- src/app/components/pages/property/property-admin/property-manage-tenants/property-manage-tenants.component.ts
+++ src/app/components/pages/property/property-admin/property-manage-tenants/property-manage-tenants.component.ts
@@ -7,6 +7,7 @@ import {ActivatedRoute} from '@angular/router';
 import {
   PropertyService,
 } from '../../../../../services/property/property.service';
+import {ModalService} from '../../../../../services/modal/modal.service';
 
 @Component({
   selector: 'app-property-manage-tenants',
@@ -19,6 +20,7 @@ export class PropertyManageTenantsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private propertyService: PropertyService,
+    private modalService: ModalService,
   ) {
   }
 
@@ -37,6 +39,38 @@ export class PropertyManageTenantsComponent implements OnInit {
         .removeTenantFromProperty(this.property.id, tenantEmailToRemove)
         .subscribe((_) => {
           this.ngOnInit();
+        });
+  }
+
+  escalateTenantInProperty(tenantEmailToEscalate: string) {
+    this
+        .propertyService
+        .escalateTenantInProperty(this.property.id, tenantEmailToEscalate)
+        .subscribe((status) => {
+          let statusMessage = 'Failed to escalate tenants privileges';
+          if (status.status === 'SUCCESS') {
+            statusMessage = 'Successfully escalated tenants privileges';
+            this.ngOnInit();
+          }
+          this.modalService.showModal('Request Status', statusMessage);
+        });
+  }
+
+  isTenantAdmin(tenantEmailToCheck: string) {
+    return this.property.administratorEmails.includes(tenantEmailToCheck);
+  }
+
+  deescalateTenantInProperty(tenantEmailToDeescalate: string) {
+    this
+        .propertyService
+        .deescalateTenantInProperty(this.property.id, tenantEmailToDeescalate)
+        .subscribe((status) => {
+          let statusMessage = 'Failed to deescalate tenants privileges';
+          if (status.status === 'SUCCESS') {
+            statusMessage = 'Successfully deescalated tenants privileges';
+            this.ngOnInit();
+          }
+          this.modalService.showModal('Request Status', statusMessage);
         });
   }
 }
