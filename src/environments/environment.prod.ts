62d91a2a24b47364a332ea2f6fbd86325e2f2823
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-06-09 00:04:27 +0100

    Updated production API URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  MAIN_API_URL: 'https://api.split.jonathanlee.io/api',
-  FRONT_END_API_URL: 'https://api.split.jonathanlee.io/api',
+  MAIN_API_URL: 'https://api.split.direct/api',
+  FRONT_END_API_URL: 'https://api.split.direct/api',
 };
